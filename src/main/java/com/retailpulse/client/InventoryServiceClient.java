23dbd11ab3a7ccbc5027cd9d356937c38e7e9fea
Casper Yap <casperyap@live.com.sg>
2025-09-25 19:43:28 +0800

    Solve jwt bug by including the Feign configuration in the FeignClient.

--- src/main/java/com/retailpulse/client/InventoryServiceClient.java
+++ src/main/java/com/retailpulse/client/InventoryServiceClient.java
@@ -6,9 +6,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "inventory-service", url = "${inventory-service.url}")
-public interface InventoryServiceClient {
-    
+@FeignClient(
+  name = "inventory-service", 
+  url = "${inventory-service.url}",
+  configuration = com.retailpulse.config.FeignConfig.class
+  )
+public interface InventoryServiceClient {    
     @PostMapping("/api/inventory/salesUpdate")
     void updateStocks(@RequestBody InventoryUpdateRequestDto request);
 }
