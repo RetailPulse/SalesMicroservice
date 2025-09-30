7bebeb38e20851296b3e93ede9deaeef301042d1
William Siling <e1503302@u.nus.edu>
2025-09-30 22:54:09 +0800

    correct commit without application.yaml file

--- src/main/java/com/retailpulse/repository/SalesTransactionRepository.java
+++ src/main/java/com/retailpulse/repository/SalesTransactionRepository.java
@@ -4,6 +4,9 @@ import com.retailpulse.entity.SalesTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SalesTransactionRepository extends JpaRepository<SalesTransaction, Long> {
+    Optional<SalesTransaction> findByPaymentId(Long paymentId);
 }
