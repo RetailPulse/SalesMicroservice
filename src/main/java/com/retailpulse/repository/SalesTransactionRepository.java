0d80071e65c602bac347876fc9b875dcbfca581b
William Siling <e1503302@u.nus.edu>
2025-10-01 10:36:49 +0800

    revert to findSalesTransaction by id

--- src/main/java/com/retailpulse/repository/SalesTransactionRepository.java
+++ src/main/java/com/retailpulse/repository/SalesTransactionRepository.java
@@ -4,9 +4,6 @@ import com.retailpulse.entity.SalesTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface SalesTransactionRepository extends JpaRepository<SalesTransaction, Long> {
-    Optional<SalesTransaction> findByPaymentId(Long paymentId);
 }
